5913a7bde761712155f1478a32bbf8aaf9ba22ef
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-11-22 22:50:50 +0530

    Binary search

---
+++ BSTUC_1/BSTUC_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BST_UC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree obj1 = new BinaryTree();
+            obj1.Insert(56);
+            obj1.Insert(30);
+            obj1.Insert(70);
+            obj1.TraverseInorder(obj1.root);
+            Console.WriteLine();
+            obj1.TraversePostorder(obj1.root);
+            Console.WriteLine();
+            obj1.TraversePreorder(obj1.root);
+        }
+    }
+}
---
+++ BSTUC_1/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BST_UC1
+{
+    public class Node
+    {
+        public int data;
+        public Node LeftNode;
+        public Node RightNode;
+        public Node()
+        {
+            this.LeftNode = null;
+            this.RightNode = null;
+        }
+
+    }
+}
