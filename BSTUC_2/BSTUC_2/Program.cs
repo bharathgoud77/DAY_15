0c95f57af22d0c9ec45a633b627b8906d2b4ec81
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-11-23 18:55:31 +0530

    BST

---
+++ BSTUC_2/BSTUC_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BTS_UC2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree obj = new BinaryTree();
+            obj.Insert(56);
+            obj.Insert(30);
+            obj.Insert(70);
+            obj.Insert(22);
+            obj.Insert(40);
+            obj.Insert(60);
+            obj.Insert(95);
+            obj.Insert(11);
+            obj.Insert(65);
+            obj.Insert(3);
+            obj.Insert(16);
+            obj.Insert(63);
+            obj.Insert(67);
+
+            obj.TraversePreorder(obj.root);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ BSTUC_2/BSTUC_2/node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BTS_UC2
+{
+    public class Node
+    {
+        public int data;
+        public Node LeftNode;
+        public Node RightNode;
+        public Node()
+        {
+            this.LeftNode = null;
+            this.RightNode = null;
+        }
+    }
+}
